ff7874d204b06d5f70928ce939299a24e9713776
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-02 20:24:23 +0530

    Converting BookController to a RestController.

--- src/main/java/com/gsr/library/libraryapp/controller/BookController.java
+++ src/main/java/com/gsr/library/libraryapp/controller/BookController.java
@@ -1,22 +1,24 @@
 package com.gsr.library.libraryapp.controller;
 
 import com.gsr.library.libraryapp.domain.Book;
-import com.gsr.library.libraryapp.repositories.BookRepository;
 import com.gsr.library.libraryapp.services.BookService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Set;
 
-@Controller
+@RestController
+@RequestMapping(path = "api/book")
 public class BookController {
     private final BookService bookService;
 
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
-
+    @GetMapping
     public List<Book> getBooks() {
-        return (List<Book>) bookService.getBooks();
+        return bookService.getBooks();
     }
+
 }
