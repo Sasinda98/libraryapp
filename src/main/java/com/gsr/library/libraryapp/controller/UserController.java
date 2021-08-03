24c171eab5464e77ac7e2345e4c0c2676f912b81
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-03 12:34:51 +0530

    Changing UserController to a RestController.

--- src/main/java/com/gsr/library/libraryapp/controller/UserController.java
+++ src/main/java/com/gsr/library/libraryapp/controller/UserController.java
@@ -4,10 +4,14 @@ import com.gsr.library.libraryapp.domain.User;
 import com.gsr.library.libraryapp.repositories.UserRepository;
 import com.gsr.library.libraryapp.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping(path = "/users")
 public class UserController {
 
     private final UserService userService;
@@ -15,9 +19,9 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
+    @GetMapping
     public List<User> getUsers(){
-        return (List<User>) userService.getUsers();
+        return userService.getUsers();
     }
 
 }
