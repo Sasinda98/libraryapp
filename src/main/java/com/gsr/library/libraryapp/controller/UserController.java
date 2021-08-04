2bfe804a3faa555bd84cc73fdd43e9cc4112f612
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-04 18:13:01 +0530

    Interface for UserService added.

--- src/main/java/com/gsr/library/libraryapp/controller/UserController.java
+++ src/main/java/com/gsr/library/libraryapp/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.gsr.library.libraryapp.controller;
 
 import com.gsr.library.libraryapp.domain.User;
-import com.gsr.library.libraryapp.repositories.UserRepository;
-import com.gsr.library.libraryapp.services.UserService;
-import org.springframework.stereotype.Controller;
+import com.gsr.library.libraryapp.services.UserServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +12,14 @@ import java.util.List;
 @RequestMapping(path = "/users")
 public class UserController {
 
-    private final UserService userService;
+    private final UserServiceImpl userServiceImpl;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
     }
     @GetMapping
     public List<User> getUsers(){
-        return userService.getUsers();
+        return userServiceImpl.getUsers();
     }
 
 }
--- src/main/java/com/gsr/library/libraryapp/services/UserService.java
+++ src/main/java/com/gsr/library/libraryapp/services/UserService.java
@@ -1,22 +1,9 @@
 package com.gsr.library.libraryapp.services;
 
 import com.gsr.library.libraryapp.domain.User;
-import com.gsr.library.libraryapp.repositories.UserRepository;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
-@Service
-public class UserService {
-
-    private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    public List<User> getUsers(){
-        return (List<User>) userRepository.findAll();
-    }
-
+public interface UserService {
+    Optional<User> getUserByID(Long userID);
 }
