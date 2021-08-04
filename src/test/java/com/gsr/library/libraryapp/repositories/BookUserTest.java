40fcb02219e3db45d3294834e36daeaa59cf64ec
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-04 11:26:31 +0530

    Refactor of getBorrowersForABookByBookID() test.

--- src/test/java/com/gsr/library/libraryapp/repositories/BookUserTest.java
+++ src/test/java/com/gsr/library/libraryapp/repositories/BookUserTest.java
@@ -26,33 +26,33 @@ public class BookUserTest {
     }
 
     @Test
-    void getBorrowersForABookByBookID(){
+    void getBorrowersForABookByBookID() {
         //given
         Book book1 = new Book("Spring Framework for Dummies", "Educational", 1, 1111);
         Book book2 = new Book("JPA for Dummies", "Educational", 3, 2222);
-        User user = new User("Gayal", "Rupasinghe", "gayal@domain.com");
-        user.getBorrowedBooks().add(book1);
-        book1.getBorrowers().add(user);
+        User user1 = new User("Gayal", "Rupasinghe", "gayal@domain.com");
+        user1.getBorrowedBooks().add(book1);
+        book1.getBorrowers().add(user1);
 
-        testUserRepository.save(user);
+        testUserRepository.save(user1);
         testBookRepository.saveAll(List.of(book1, book2));
 
         //when
         ArrayList<User> book1Borrowers = (ArrayList<User>) testBookRepository.getBorrowersForABookByBookID(book1.getBookID());
         ArrayList<User> book2Borrowers = (ArrayList<User>) testBookRepository.getBorrowersForABookByBookID(book2.getBookID());
 
-        //then
         //Expect borrower to be user.
         Long borrowerCountBook1 = book1Borrowers
                 .stream()
-                .filter(u -> u.getUserID() == user.getUserID())
+                .filter(u -> u.getUserID().equals(user1.getUserID()))
                 .count();
-        assertThat(borrowerCountBook1).isEqualTo(1L);
-
         //Expect to have no borrowers.
         Long borrowerCountBook2 = book2Borrowers
                 .stream()
                 .count();
+
+        //then
+        assertThat(borrowerCountBook1).isEqualTo(1L);
         assertThat(borrowerCountBook2).isEqualTo(0L);
     }
 }
