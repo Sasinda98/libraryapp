f87da2f612fa0907de95251d42fbddf2e6dc71d3
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-05 18:07:21 +0530

    Added endpoint to be able to get list of books borrowed by a specific user.

--- src/main/java/com/gsr/library/libraryapp/controller/UserController.java
+++ src/main/java/com/gsr/library/libraryapp/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.gsr.library.libraryapp.controller;
 
+import com.gsr.library.libraryapp.domain.Book;
 import com.gsr.library.libraryapp.domain.User;
+import com.gsr.library.libraryapp.domain.dto.BookDto;
+import com.gsr.library.libraryapp.domain.dto.ListBookDto;
+import com.gsr.library.libraryapp.domain.dto.ListUserDto;
 import com.gsr.library.libraryapp.services.UserServiceImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -14,13 +21,23 @@ public class UserController {
 
     private final UserServiceImpl userServiceImpl;
 
-    public UserController(UserServiceImpl userServiceImpl) {
+    private final ModelMapper modelMapper;
+
+    public UserController(UserServiceImpl userServiceImpl, ModelMapper modelMapper) {
         this.userServiceImpl = userServiceImpl;
+        this.modelMapper = modelMapper;
     }
+
     @GetMapping
     public List<User> getUsers(){
         return userServiceImpl.getUsers();
     }
-
+    
+    @GetMapping("/{user_id}/books")
+    public ListBookDto getBorrowersForABook(@PathVariable(name = "user_id") Long userID){
+        List<Book> books = userServiceImpl.getBooksBorrowedByUserID(userID);
+        List<BookDto> bookDtos = books.stream().map(book -> modelMapper.map(book, BookDto.class)).collect(Collectors.toList());
+        return new ListBookDto(bookDtos.size(), bookDtos);
+    }
 }
 
