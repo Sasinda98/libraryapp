04bda05404ed79a31e2b38bc4daca3d17d3d88f3
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-05 17:55:10 +0530

    Bringing in ModelMapper dependency.

--- src/main/java/com/gsr/library/libraryapp/LibraryappApplication.java
+++ src/main/java/com/gsr/library/libraryapp/LibraryappApplication.java
@@ -1,7 +1,9 @@
 package com.gsr.library.libraryapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class LibraryappApplication {
@@ -10,4 +12,8 @@ public class LibraryappApplication {
 		SpringApplication.run(LibraryappApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 }
