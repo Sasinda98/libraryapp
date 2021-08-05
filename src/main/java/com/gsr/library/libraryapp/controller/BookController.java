76dd77769c1ca738f3fb07b33e7c0e0a84a8f032
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-05 17:58:22 +0530

    Endpoint to get list of borrowers for a specific book added.

--- src/main/java/com/gsr/library/libraryapp/controller/BookController.java
+++ src/main/java/com/gsr/library/libraryapp/controller/BookController.java
@@ -1,24 +1,38 @@
 package com.gsr.library.libraryapp.controller;
 
 import com.gsr.library.libraryapp.domain.Book;
+import com.gsr.library.libraryapp.domain.User;
+import com.gsr.library.libraryapp.domain.dto.ListUserDto;
+import com.gsr.library.libraryapp.domain.dto.UserDto;
 import com.gsr.library.libraryapp.services.BookServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(path = "api/book")
+@RequestMapping(path = "/books")
 public class BookController {
     private final BookServiceImpl bookServiceImpl;
+    private final ModelMapper modelMapper;
 
-    public BookController(BookServiceImpl bookServiceImpl) {
+    public BookController(BookServiceImpl bookServiceImpl, ModelMapper modelMapper) {
         this.bookServiceImpl = bookServiceImpl;
+        this.modelMapper = modelMapper;
     }
+
     @GetMapping
     public List<Book> getBooks() {
         return bookServiceImpl.getAllBooks();
     }
 
+    @GetMapping("/{book_id}/borrowers")
+    public ListUserDto getBorrowersForABook(@PathVariable(name = "book_id") Long book_id){
+        List<User> users = bookServiceImpl.getBorrowersForABookByBookID(book_id);
+        List<UserDto> userDtos = users.stream().map(user1 -> modelMapper.map(user1, UserDto.class)).collect(Collectors.toList());
+        return new ListUserDto(userDtos.size(), userDtos);
+    }
 }
