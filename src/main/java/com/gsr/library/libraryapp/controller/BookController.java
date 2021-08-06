29bb03059c6cc6426324c15b29987d59757cd306
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-06 12:51:14 +0530

    Adding endpoint to borrow books.

--- src/main/java/com/gsr/library/libraryapp/controller/BookController.java
+++ src/main/java/com/gsr/library/libraryapp/controller/BookController.java
@@ -5,11 +5,14 @@ import com.gsr.library.libraryapp.domain.User;
 import com.gsr.library.libraryapp.domain.dto.BookDto;
 import com.gsr.library.libraryapp.domain.dto.ListUserDto;
 import com.gsr.library.libraryapp.domain.dto.UserDto;
+import com.gsr.library.libraryapp.exceptions.ValidationException;
 import com.gsr.library.libraryapp.services.BookServiceImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -39,6 +42,23 @@ public class BookController {
     public BookDto deleteABook(@PathVariable("book_id") Long bookID){
         Book deletedBook = bookServiceImpl.deleteBook(bookID);
         return modelMapper.map(deletedBook, BookDto.class);
+    }
+
+    @PostMapping(value = "/borrow-info")
+    public Map<String, Object> borrowBook(@RequestBody Map<String, Object> payload){
+        try {
+            Long userID = ((Number) payload.get("user_id")).longValue();
+            Long bookID = ((Number) payload.get("book_id")).longValue();
+            bookServiceImpl.borrowBook(userID, bookID);
+        }
+        catch (ClassCastException | NullPointerException ex){
+            //When user_id book_id in req body is not in type we expect.
+            //Or the keys don't exist at all.
+            throw new ValidationException("Bad request received.");
+        }
+        HashMap<String, Object> success = new HashMap<>();
+        success.put("message", "Book successfully borrowed.");
+        return success;
     }
 
 }
