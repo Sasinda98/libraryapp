ba05c971d3f79f90de3c676d28d2a62138e05d24
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-06 19:57:41 +0530

    Moving exception handlers to a different package/folder.

--- src/main/java/com/gsr/library/libraryapp/exceptions/handlers/APIOperationStoppedExceptionHandler.java
+++ src/main/java/com/gsr/library/libraryapp/exceptions/handlers/APIOperationStoppedExceptionHandler.java
@@ -1,5 +1,6 @@
-package com.gsr.library.libraryapp.exceptions;
+package com.gsr.library.libraryapp.exceptions.handlers;
 
+import com.gsr.library.libraryapp.exceptions.OperationStoppedException;
 import com.gsr.library.libraryapp.exceptions.exceptiontemplates.APIExceptionTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.util.Date;
 
 @ControllerAdvice
-public class APIValidationExceptionHandler {
-    @ExceptionHandler(value = {ValidationException.class})
-    public ResponseEntity<Object> handleAPIException(ValidationException ex){
-        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+public class APIOperationStoppedExceptionHandler {
+    @ExceptionHandler(value = {OperationStoppedException.class})
+    public ResponseEntity<Object> handleAPIException(OperationStoppedException ex){
+        HttpStatus httpStatus = HttpStatus.CONFLICT;
         APIExceptionTemplate exceptionDetailsToExpose = new APIExceptionTemplate(ex.getMessage(), httpStatus.getReasonPhrase(),
                 httpStatus.value(), new Date());
         return new ResponseEntity<>(exceptionDetailsToExpose, httpStatus);
