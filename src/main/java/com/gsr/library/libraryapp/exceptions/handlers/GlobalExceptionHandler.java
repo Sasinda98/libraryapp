5e482bc80cfa88d341e3df1031aaf568dc782c27
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-11 15:35:21 +0530

    Make all exceptions thrown conform to same API exception response format.

---
+++ src/main/java/com/gsr/library/libraryapp/exceptions/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.gsr.library.libraryapp.exceptions.handlers;
+
+import com.gsr.library.libraryapp.exceptions.ValidationException;
+import com.gsr.library.libraryapp.exceptions.exceptiontemplates.APIExceptionTemplate;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Locale;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    /***
+     * Conform server thrown exceptions also to APIExceptionTemplate defined.
+     */
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String servletPath = ((ServletWebRequest) request).getRequest().getRequestURI();
+        APIExceptionTemplate exceptionDetailsToExpose = new APIExceptionTemplate("Something went wrong", status.getReasonPhrase(), servletPath,
+                status.value(), new Date());
+        return new ResponseEntity<>(exceptionDetailsToExpose, status);
+    }
+}
