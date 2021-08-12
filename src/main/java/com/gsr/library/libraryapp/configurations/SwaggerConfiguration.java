0bfe09f64e7b56435a32c1c94ee8aa885339b62f
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-12 10:50:22 +0530

    Added API info.

--- src/main/java/com/gsr/library/libraryapp/configurations/SwaggerConfiguration.java
+++ src/main/java/com/gsr/library/libraryapp/configurations/SwaggerConfiguration.java
@@ -2,8 +2,11 @@ package com.gsr.library.libraryapp.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,6 +24,19 @@ public class SwaggerConfiguration {
                 .paths(PathSelectors.any())
                 .build()
                 .useDefaultResponseMessages(false)  //prevent swagger from putting default response messages.
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(apiInfo());
     }
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("API that models library.")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .termsOfServiceUrl("TOS here.")
+                .version("1.0.0")
+                .build();
+    }
+
 }
