d124d5448b55f28b2ce692bf5231cc72b1550021
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-16 12:35:01 +0530

    removing last friday's tryouts and config for today's.

--- src/main/java/com/gsr/library/libraryapp/LibraryappApplication.java
+++ src/main/java/com/gsr/library/libraryapp/LibraryappApplication.java
@@ -4,8 +4,10 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class LibraryappApplication {
 
 	public static void main(String[] args) {
