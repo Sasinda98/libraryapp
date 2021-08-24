bf14f24f346c7484fd6ebca4eac75ca426b91fcb
Sasinda98 <sasindarupasighe@icloud.com>
2021-08-24 10:19:20 +0530

    cleanup.

--- src/main/java/com/gsr/library/libraryapp/LibraryappApplication.java
+++ src/main/java/com/gsr/library/libraryapp/LibraryappApplication.java
@@ -7,11 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.web.servlet.oauth2.resourceserver.OAuth2ResourceServerSecurityMarker;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-//@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @OpenAPIDefinition(info = @Info(title = "Libaray API", description = "This API models a library.", version = "1.0.0"))
 public class LibraryappApplication {
